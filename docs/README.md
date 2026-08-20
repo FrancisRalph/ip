ebf62c4d720d8b544f45ccbd9b8d93ccf1f0e5f3
FrancisRalph <francis341593@gmail.com>
2026-08-21 01:33:47 +0800

    update chatbot name from Duke to Ralph

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Ralph User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Ralph.java
@@ -0,0 +1,13 @@
+public class Ralph {
+    public static void main(String[] args) {
+        String banner = """
+                 ____       _      _        ____    _   _\s
+                |  _ \\     / \\    | |      |  _ \\  | | | |
+                | |_) |   / _ \\   | |      | |_) | | |_| |
+                |  _ <   / ___ \\  | |___   |  __/  |  _  |
+                |_| \\_\\ /_/   \\_\\ |_____|  |_|     |_| |_|
+                Hello! I'm Ralph.
+                """;
+        System.out.println(banner);
+    }
+}
