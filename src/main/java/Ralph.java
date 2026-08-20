d3a2e5f600f3a2aafff540b832f19d2b472e368b
FrancisRalph <francis341593@gmail.com>
2026-08-21 01:51:22 +0800

    add interactivity (echo input, exit command)

--- src/main/java/Ralph.java
+++ src/main/java/Ralph.java
@@ -1,15 +1,39 @@
+/**
+ * Simple interactive CLI that echoes user commands and exits on 'bye'.
+ */
 public class Ralph {
+    /**
+     * Entry point: prints a header, then reads lines from stdin.
+     * Echoes each command back to the user and stops when the user types "bye".
+     */
     public static void main(String[] args) {
         String banner = """
-                 ____       _      _        ____    _   _\s
+                 ____       _      _        ____    _   _
                 |  _ \\     / \\    | |      |  _ \\  | | | |
                 | |_) |   / _ \\   | |      | |_) | | |_| |
                 |  _ <   / ___ \\  | |___   |  __/  |  _  |
                 |_| \\_\\ /_/   \\_\\ |_____|  |_|     |_| |_|
                 Hello! I'm Ralph.
-                What can I do for you?
-                Bye. Hope to see you again soon!
                 """;
         System.out.println(banner);
+
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        final String SEP = "____________________________________________________________";
+
+        System.out.println("What can I do for you?");
+
+        while (true) {
+            String line = scanner.nextLine();
+            System.out.println(SEP);
+            if (line.trim().equalsIgnoreCase("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(SEP);
+                break;
+            } else {
+                System.out.println(" " + line);
+                System.out.println(SEP);
+            }
+        }
+        scanner.close();
     }
 }
